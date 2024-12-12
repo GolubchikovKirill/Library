c4d83eb3398bbcdd64e685b3b77557ee20bd4642
GolubchikovKirill <iam@kgolubchikov.ru>
2024-12-12 13:44:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title} - {self.author} - {self.year}"
+
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = None
+        self._password = None
+        self._email = None
+
+class Library:
+    def __init__(self):
+        self.books = []
+        self.users = []
+
+    def add_book(self, book):
+        if book not in self.books:
+            self.books.append(book)
+            self.users.append(User(book.first_name, book.last_name))
+            return True
+        else:
+            return False
